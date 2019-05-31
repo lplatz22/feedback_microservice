e01db5b2d3bea5ba9dcd73a393273896b4db4d2f
Luke Platz <lplatz@umich.edu>
2019-05-31 17:17:57 -0500

    working feedback app

--- src/main/java/io/swagger/Swagger2SpringBoot.java
+++ src/main/java/io/swagger/Swagger2SpringBoot.java
@@ -5,6 +5,7 @@ import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
--- src/main/java/io/swagger/api/FeedbackApiController.java
+++ src/main/java/io/swagger/api/FeedbackApiController.java
@@ -14,6 +14,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
@@ -23,6 +25,12 @@ import javax.validation.Valid;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.List;
+import com.cloudant.client.api.Database;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.cloudant.client.api.model.Response;
+import java.util.ArrayList;
+import java.util.Date;
+
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-05-31T21:52:44.949Z")
 
 @Controller
@@ -34,38 +42,55 @@ public class FeedbackApiController implements FeedbackApi {
 
     private final HttpServletRequest request;
 
+    @Autowired
+private Database feedbackDB;
+
+
     @org.springframework.beans.factory.annotation.Autowired
     public FeedbackApiController(ObjectMapper objectMapper, HttpServletRequest request) {
         this.objectMapper = objectMapper;
         this.request = request;
     }
 
-    public ResponseEntity<SuccessfulResponse> createFeedback(@ApiParam(value = "Pet object that needs to be added to the store" ,required=true )  @Valid @RequestBody FeedbackInput body) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<SuccessfulResponse>(objectMapper.readValue("{  \"rev\" : \"rev\",  \"id\" : \"id\"}", SuccessfulResponse.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<SuccessfulResponse>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        return new ResponseEntity<SuccessfulResponse>(HttpStatus.NOT_IMPLEMENTED);
-    }
-
     public ResponseEntity<List<Feedback>> getFeedback() {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
+        List<Feedback> allDocs = new ArrayList<Feedback>();
+        try {
+            allDocs = feedbackDB.getAllDocsRequestBuilder()
+                                .includeDocs(true)
+                                .build()
+                                .getResponse()
+                                .getDocsAs(Feedback.class);
+        } catch (Exception e) {
+            log.error("error occurred: {}", e.getLocalizedMessage());
+            return new ResponseEntity<> (HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<List<Feedback>>(allDocs, HttpStatus.OK);
+    }
+
+    public ResponseEntity<SuccessfulResponse>
+        createFeedback(@ApiParam(value = ""  )  @Valid @RequestBody
+        FeedbackInput body) {
+            Response response;
+            SuccessfulResponse successfulResponse;
+            Feedback newFeedback = new Feedback();
+            Date d = new Date();
+            newFeedback.setCreatedTs(d.toString());
+            newFeedback.setFeedback(body.getFeedback());
+            newFeedback.setName(body.getName());
+            newFeedback.setRating(body.getRating());
             try {
-                return new ResponseEntity<List<Feedback>>(objectMapper.readValue("[ {  \"feedback\" : \"feedback\",  \"rev\" : \"rev\",  \"created_ts\" : \"created_ts\",  \"name\" : \"name\",  \"rating\" : 0,  \"id\" : \"id\"}, {  \"feedback\" : \"feedback\",  \"rev\" : \"rev\",  \"created_ts\" : \"created_ts\",  \"name\" : \"name\",  \"rating\" : 0,  \"id\" : \"id\"} ]", List.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<List<Feedback>>(HttpStatus.INTERNAL_SERVER_ERROR);
+                response = feedbackDB.save(newFeedback);
+                successfulResponse = new SuccessfulResponse();
+                successfulResponse.setId(response.getId());
+                successfulResponse.setRev(response.getRev());
+            } catch (Exception e) {
+                log.error("error occurred: {}", e.getLocalizedMessage());
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
             }
+            ResponseEntity<SuccessfulResponse> responseEntity = new ResponseEntity<SuccessfulResponse>(successfulResponse, HttpStatus.OK);
+            return responseEntity;
         }
 
-        return new ResponseEntity<List<Feedback>>(HttpStatus.NOT_IMPLEMENTED);
-    }
+    
 
 }
